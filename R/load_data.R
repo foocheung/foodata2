191f1aaa9e16f9d2910f74978fad4f7f2221ba73
Cheung <cheungf@L02256598.niaid.nih.gov>
2025-08-26 09:31:06 -0400

    Describe your update here

--- R/load_data.R
+++ R/load_data.R
@@ -22,6 +22,6 @@ load_data3B <- function() {
 
 load_data4 <- function() {
   data <- readxl::read_xlsx(system.file("data", "synthetic_data.xlsx", package = "foodata2"))
-
+  data <- as.data.frame(data, check.names = FALSE)
   return(data)
 }
